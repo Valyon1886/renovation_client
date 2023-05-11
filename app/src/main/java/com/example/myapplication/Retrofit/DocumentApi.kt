d67eccf1b35b1302485ee416f315901bed479215
Valyon1886 <64031088+valyon1886@users.noreply.github.com>
2023-05-11 18:30:51 +0300

    Загрузка файла с сервера

--- app/src/main/java/com/example/myapplication/Retrofit/DocumentApi.kt
+++ app/src/main/java/com/example/myapplication/Retrofit/DocumentApi.kt
@@ -2,11 +2,22 @@ package com.example.myapplication.Retrofit
 
 import com.example.myapplication.Entity.Document
 import com.example.myapplication.Entity.User
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable.Param
+import okhttp3.ResponseBody
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.Streaming
 
 interface DocumentApi {
     @POST("/api/doc/add/{jobId}")
     suspend fun generateDoc(@Body user: User, @Path("jobId") jobId: Int)
+    @GET("/api/doc/getAllFileNames/{userId}")
+    suspend fun getAllFileNames(@Path("userId") userId: Int): MutableList<String>
+    @GET("/api/doc/getFile/{fileName}")
+    @Streaming
+    suspend fun downloadFile(@Path("fileName") fileName: String): Response<ResponseBody>
 }
--- app/src/main/java/com/example/myapplication/ScreenDocx/ScreenDocx.kt
+++ app/src/main/java/com/example/myapplication/ScreenDocx/ScreenDocx.kt
@@ -2,21 +2,26 @@ package com.example.myapplication.ScreenDocx
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.Entity.MainMaterial
 import com.example.myapplication.Entity.User
 import com.example.myapplication.MainActivity
+import com.example.myapplication.R
 import com.example.myapplication.Retrofit.DocumentApi
 import com.example.myapplication.ui.theme.BGColor
 import com.example.myapplication.ui.theme.NavColor
@@ -24,10 +29,17 @@ import com.example.myapplication.ui.theme.Red
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun ScreenDocx(documentApi: DocumentApi, mainActivity: MainActivity, user: User) {
+    var listDocument by remember { mutableStateOf<List<String>>(emptyList()) }
+
+    LaunchedEffect(true) {
+        listDocument = documentApi.getAllFileNames(user.id)
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -48,7 +60,70 @@ fun ScreenDocx(documentApi: DocumentApi, mainActivity: MainActivity, user: User)
                     .fillMaxSize()
                     .background(color = BGColor)
             ){
+                LazyColumn {
+                    itemsIndexed(listDocument) { _, item ->
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            backgroundColor = NavColor,
+                            shape = RoundedCornerShape(10.dp),
+                            elevation = 10.dp
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(5.dp)
+                            ) {
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween,
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Box(
+                                        contentAlignment = Alignment.CenterStart,
+                                        modifier = Modifier.weight(6f)
+                                    ) {
+                                        Text(
+                                            text = item,
+                                            fontWeight = FontWeight.Bold,
+                                            fontSize = 20.sp,
+                                            color = Color.White
+                                        )
+                                    }
+                                    Box(
+                                        contentAlignment = Alignment.CenterStart,
+                                        modifier = Modifier.weight(1f)
+                                    ) {
+                                        IconButton(
+                                            onClick = {
+                                                CoroutineScope(Dispatchers.IO).launch {
 
+                                                    val response = documentApi.downloadFile(item)
+                                                    val inputStream = response.body()?.byteStream()
+                                                    val file = File("/storage/emulated/0/Download/", "$item.docx")
+                                                    val outputStream = FileOutputStream(file)
+
+                                                    inputStream?.use { input ->
+                                                        outputStream.use { output ->
+                                                            input.copyTo(output)
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        ){
+                                            Icon(
+                                                painterResource(id = R.drawable.baseline_arrow_circle_down_24),
+                                                contentDescription = "Скачать документ",
+                                                tint = Color.White
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     )
